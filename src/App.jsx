89e7e12cd843c3bfb66095cf271629abe1330f62
Andres Laguilavo <lagui2003@gmail.com>
2022-11-17 16:01:13 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './index.css'
+
+function App() {
+
+  return (
+<>
+<h1>hola mundo</h1>
+</>
+
+  )
+}
+
+export default App
